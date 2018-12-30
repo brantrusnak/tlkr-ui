661c00a1658aa4eb8707126a424743f5be4aee60
Brant <brantrusnakx95@gmail.com>
2018-12-29 23:08:55 -0500

    Added State and EventHandlers to SignIn and SignUp.

--- src/components/auth/SignIn.tsx
+++ src/components/auth/SignIn.tsx
@@ -1,34 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component, FormEvent, FormEventHandler } from 'react'
 import {updateTextFields} from 'materialize-css';
 
 export default class SignIn extends Component {
+  state = {
+    username: '',
+    password: ''
+  }
+  handleChange: FormEventHandler;
+  handleSubmit: FormEventHandler;
+
+  constructor(props: any) {
+    super(props)
+    
+    this.handleChange = (event: FormEvent<HTMLInputElement>) => {
+      this.setState({
+        [event.currentTarget.id]: event.currentTarget.value
+      });
+    }
+
+    this.handleSubmit = (event: FormEvent) => {
+      event.preventDefault();
+      console.log(event.currentTarget);
+    }
+    
+  }
+
   componentDidMount() {
     updateTextFields();
   }
+
   render() {
     return (
       <div className="container">
         <div className="card hoverable">
           <div className="row">
-            <form className="col s12">
+            <form onSubmit={this.handleSubmit} className="col s12">
               <div className="row">
-
                 <div className="input-field col s12">
                   <i className="material-icons prefix">account_circle</i>
-                  <input id="username" type="text" className="validate" />
                   <label htmlFor="username">Username</label>
+                  <input onChange={this.handleChange} id="username" type="text" className="validate" />
                 </div>
-
                 <div className="input-field col s12">
                   <i className="material-icons prefix">account_circle</i>
-                  <input id="password" type="text" className="validate" />
+                  <input onChange={this.handleChange} id="password" type="password" className="validate" />
                   <label htmlFor="password">Password</label>
                 </div>
-
                 <button className="btn btn-large waves-effect waves-light" type="submit" name="action">Submit
                   <i className="material-icons right">send</i>
                 </button>
-
               </div>
             </form>
           </div>
--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -1,10 +1,69 @@
-import React, { Component } from 'react'
+import React, { Component, FormEvent, FormEventHandler } from 'react'
+import {updateTextFields} from 'materialize-css';
 
 export default class SignUp extends Component {
+  state = {
+    username: '',
+    password: '',
+    email: '',
+    displayName: ''
+  }
+  handleChange: FormEventHandler;
+  handleSubmit: FormEventHandler;
+
+  constructor(props: any) {
+    super(props)
+    
+    this.handleChange = (event: FormEvent<HTMLInputElement>) => {
+      this.setState({
+        [event.currentTarget.id]: event.currentTarget.value
+      });
+    }
+
+    this.handleSubmit = (event: FormEvent) => {
+      event.preventDefault();
+      console.log(event.currentTarget);
+    }
+    
+  }
+
+  componentDidMount() {
+    updateTextFields();
+  }
   render() {
     return (
-      <div>
-        Sign up
+      <div className="container">
+        <div className="card hoverable">
+          <div className="row">
+            <form onSubmit={this.handleSubmit} className="col s12">
+              <div className="row">
+                <div className="input-field col s12">
+                  <i className="material-icons prefix">account_circle</i>
+                  <label htmlFor="username">Username</label>
+                  <input onChange={this.handleChange} id="username" type="text" className="validate" />
+                </div>
+                <div className="input-field col s12">
+                  <i className="material-icons prefix">account_circle</i>
+                  <input onChange={this.handleChange} id="password" type="password" className="validate" />
+                  <label htmlFor="password">Password</label>
+                </div>
+                <div className="input-field col s12">
+                  <i className="material-icons prefix">account_circle</i>
+                  <input onChange={this.handleChange} id="email" type="email" className="validate" />
+                  <label htmlFor="email">Email</label>
+                </div>
+                <div className="input-field col s12">
+                  <i className="material-icons prefix">account_circle</i>
+                  <input onChange={this.handleChange} id="displayName" type="text" className="validate" />
+                  <label htmlFor="displayName">Display Name</label>
+                </div>
+                <button className="btn btn-large waves-effect waves-light" type="submit" name="action">Submit
+                  <i className="material-icons right">send</i>
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     )
   }
