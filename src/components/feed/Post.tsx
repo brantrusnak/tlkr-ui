1039f7cdbb7db9e361e039e658f8a05bd70240fd
Brant <brantrusnakx95@gmail.com>
2019-05-22 14:24:53 -0400

    Pushing latest

--- src/components/feed/Post.tsx
+++ src/components/feed/Post.tsx
@@ -1,21 +1,47 @@
 import React from 'react'
-
-// id	1
-// text	Hello!
-// favoriteCount	0
-// postedBy	1
-// isShowcase	false
-// createdAt	2019-02-26T17:28:44.000Z
-// updatedAt	2019-02-26T17:28:44.000Z
+import HTTPUtil from '../util/HTTPUtil';
 
 interface PostProps{
+  post:{
+    id: number;
     text: string;
+    favoriteCount: number;
+    postedBy: number;
+    isShowcase: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+  }
 }
 
 export default function Post(props: PostProps) {
+
+  let getDate = () => {
+    let postedDate = new Date(props.post.createdAt);
+    let time = Date.now() - postedDate.getTime();
+    return (Math.round(Math.floor(time / 1000) / 60));
+  }
+
+  let getUser = async () => {
+    let http = new HTTPUtil();
+    let data = await http.GET(`http://localhost:5000/user/${props.post.postedBy}`);
+    let user = await data.json();
+    return user.response.displayName;
+  }
+
+  getUser()
+
   return (
-    <div className="z-depth-2">
-      Post: {props.text}
-    </div>
+    <li className="collection-item">
+      <div className="avatar"><i className="material-icons circle">account_circle</i></div>
+      <div className="details">
+        <div className="who">
+          <div className="username">{getUser() + ' '}</div>
+          <div className="time">{getDate() + 'm'}</div>
+        </div>
+        <div className="post">
+          {props.post.text}
+        </div>
+      </div>
+    </li>
   )
 }
--- src/components/feed/PostList.tsx
+++ src/components/feed/PostList.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import Post from './Post';
 
 interface PostListProps {
-  posts: { id: number; text: string }[];
+  posts: {
+    id: number;
+    text: string;
+    favoriteCount: number;
+    postedBy: number;
+    isShowcase: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+  }[];
 }
 
 export default function PostList(props: PostListProps) {
-  let posts = props.posts.map(post => <Post key={post.id} text={post.text} />);
-  return <div>{posts}</div>;
+  let posts = props.posts.map(post => <Post key={post.id} post={post} />);
+  return <ul className="collection">{posts}</ul>
 }
